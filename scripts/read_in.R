d971af1147eae48d693becfc307cb7c15ba66818
benmbrew <benmbrew@gmail.com>
2018-07-23 14:47:21 -0400

    created scripts for alexandar et al

--- scripts/read_in.R
+++ scripts/read_in.R
@@ -1,2 +1,36 @@
 # This script will read in data sent from SFI and clean the data if necessary, as well as a ad hoc 
 # script for exploring the data and generating new ideas
+
+
+# This document will be the main script for coding specifi plot themes for SFI, based on the specs they've sent.
+
+##########
+# Specs
+##########
+
+# - SIZE and PROPORTIONS
+# --- a) Page size: 6 in x 9 in
+# --- b) Text block size: 4 in x 7.25 in
+# --- c) Graphics: should fit snuggly in text block
+# --- d) Page size: 6 in x 9 in
+# --- e) Vector
+
+# - TEXT
+# --- a) Font: CMU Bright
+# --- b) Variables: EB Garamond italics, if possible
+# --- c) Word clouds:  CMU bright, potentially with differing degrees of boldness to provide visual cues. 
+
+# - LINES
+# --- a) Line weight: 0.5pt (at 100%)
+# --- b) Thinner lines: use tint of black instead of small lines.
+
+# - COLOR & GRADIENTS
+# --- a) All graphics should be grayscale 
+# --- b) To distinguish elements in a grayscale environment, where possible, we’d prefer to use tints rather than patterns. 
+
+# - LOOK & FEEL (FROM PLOTS)
+# --- a) Figures should not have boxs aronud them
+# --- b) 3D graphics should have perspective lines to establish a 3D sp
+
+
+
