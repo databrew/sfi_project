b394db9dbb0574a4df3dcf39c4a19bf621189bb5
benmbrew <benmbrew@gmail.com>
2018-07-25 13:29:00 -0400

    finished figure 1 from alexandar_et_al

--- scripts/alexandar_et_al_theme.R
+++ scripts/alexandar_et_al_theme.R
@@ -1,29 +1,6 @@
 
 # This script will read in data from research by Alexandar et al and create graphics with a unique theme
 
-# initialize libraries 
-library(databrew)
-library(readr)
-library(xlsx)
 
-# create an object to store the path to the data
-data_folder <- '../data/Alexander_et_al'
-
-# ----------------------------------- FIGURE 
-# read in figure 1 data set
-figure_1 <- read_csv(paste0(data_folder, '/figure_1.csv'))
-
-# the data read is filled by NA. Grab complete cases since NAs are the same across columns
-figure_1 <- figure_1[complete.cases(figure_1),]
-
-# figure_1 appears to be a table with extra columns generated from the existing data. 
-# duplicate the process to generate these new columns
-# "players", 
-
-
-# read in figure 6 data set 
-figure_6 <- read.xlsx(paste0(data_folder, '/figure_6.xlsx'), sheetName = 1)
-
-# read in figure 4 through 7 data set
-figure_4_7 <- read.xlsx(paste0(data_folder, '/figure_4_7.xlsx'), sheetName = 1)
-
+# generate a data table from DT
+DT::datatable(figure_1)
